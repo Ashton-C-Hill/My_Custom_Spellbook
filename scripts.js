808c216b883be934c21c7ad84516e7fd50653415
Ashton Hill <ashtonchill1542@gmail.com>
2024-06-01 13:45:53 -0500

    Added Event Listeners

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const buttonsOriginal = document.getElementsByClassName("desc_button")
+let buttons = Array.from(buttonsOriginal)
+buttons.forEach((button) => {button.addEventListener("click", () => {
+    
+})})
