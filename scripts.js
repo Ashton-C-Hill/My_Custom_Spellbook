5449f58bc9407061d1b1f37e02a74e6ec9fb4ebe
Ashton Hill <ashtonchill1542@gmail.com>
2024-06-01 13:59:59 -0500

    Buttons now display corresponding spell description, descriptions are hidden by default

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const buttonsOriginal = document.getElementsByClassName("desc_button")
 let buttons = Array.from(buttonsOriginal)
 buttons.forEach((button) => {button.addEventListener("click", () => {
-    
+    const desc = button.parentElement.parentElement.lastElementChild
+    if (desc.style.display === "block") {
+        desc.style.display = "none"
+    } else {
+        desc.style.display = "block"
+    }
 })})
